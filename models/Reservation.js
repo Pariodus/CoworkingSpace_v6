9b033f41669703dfa3fe4e21c88b506b66234829
Pariodus <145102769+Pariodus@users.noreply.github.com>
2024-03-03 14:11:37 +0700

    Porlaew

---
+++ models/Reservation.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ReservationSchema = new mongoose.Schema({
+    reserveDateStart:{
+        type: Date,
+        required: true
+    },
+    reserveDateEnd:{
+        type: Date,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true        
+    },
+    coworking: {
+        type: mongoose.Schema.ObjectId, 
+        ref: 'Coworking',
+        required: true
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Reservation', ReservationSchema);
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
+
+//Route files
+const coworkings = require('./routes/coworkings');
+const auth = require('./routes/auth');
+const reservations = require('./routes/reservations');
+const users = require('./routes/users')
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+//add cookie parser
+app.use(cookieParser());
+
+//Body parser
+app.use(express.json());
+
+//Sanitize
+app.use(mongoSanitize());
+
+//Set security headers (helmet)
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+    windowsMs: 10*60*1000, //10mins
+    max: 100 //sent 5 times
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
+
+//Mount routers 
+app.use('/api/v1/coworkings', coworkings);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/reservations', reservations);
+app.use('/api/v1/users', users);
+
+
+const PORT = process.env.PORT || 5000;
+
+const Server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    Server.close(()=>process.exit(1));
+});
